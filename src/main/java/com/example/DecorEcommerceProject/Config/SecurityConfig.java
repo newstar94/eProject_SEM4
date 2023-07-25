629736cd2dc0942b4ef384368e6cd7f201c19f0f
newstar94 <77623864+newstar94@users.noreply.github.com>
2023-07-25 11:54:18 +0700

    fix cors

--- src/main/java/com/example/DecorEcommerceProject/Config/SecurityConfig.java
+++ src/main/java/com/example/DecorEcommerceProject/Config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.example.DecorEcommerceProject.Security.JwtAuthEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -46,27 +45,21 @@ public class SecurityConfig {
 
             //Product
             "/api/products",
-            "/api/product/{id}"
+            "/api/product/{id}",
+
+            //User
+            "/api/users/login",
+            "/api/users/add"
     };
 
     //    private static final String[] ALLOW_POST_ALL_URLS = {
 //            "/api/category/add",
 //            "/api/products/add"
 //    };
-    private static final String[] ALLOW_GET_USER_URLS = {
+    private static final String[] ALLOW_USER_URLS = {
             "/api/users/{id}",
-
-
     };
-    private static final String[] ALLOW_GET_ADMIN_URLS = {
-            //User
-            "/api/users",
-            "/api/users/{id}",
-            "/api/users/get-by-level"
-            //"/api/user/{id}"
-    };
-    private static final String[] ALLOW_POST_ADMIN_URLS = {
-
+    private static final String[] ALLOW_ADMIN_URLS = {
             //Role
             "/api/users/role/save",
 
@@ -80,6 +73,11 @@ public class SecurityConfig {
             "/api/products/save/{id}",
             "/api/products/delete/{id}",
             "/api/products/save/{id}",
+
+            //User
+            "/api/users",
+            "/api/users/{id}",
+            "/api/users/get-by-level"
     };
 
 
@@ -90,17 +88,10 @@ public class SecurityConfig {
                 .authenticationEntryPoint(authEntryPoint)
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/api/users/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .httpBasic();
 
         http.authorizeRequests().antMatchers(ALLOW_ALL_URLS).permitAll();
-        http.authorizeRequests().antMatchers(HttpMethod.GET, ALLOW_GET_USER_URLS).hasRole("USER");
-        http.authorizeRequests().antMatchers(HttpMethod.GET, ALLOW_GET_ADMIN_URLS).hasAnyAuthority("ADMIN");
-        http.authorizeRequests().antMatchers(HttpMethod.POST, ALLOW_POST_ADMIN_URLS).hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(ALLOW_USER_URLS).hasRole("USER");
+        http.authorizeRequests().antMatchers(ALLOW_ADMIN_URLS).hasAnyAuthority("ADMIN");
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
